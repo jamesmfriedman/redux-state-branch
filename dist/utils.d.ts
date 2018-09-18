af8410be117a01ec216d42f024291b8418d76315
James Friedman <me@jamesmfriedman.com>
2018-09-18 09:53:41 -0400

    Removing untracked files

--- dist/utils.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const makeType: (prefix: string, suffix?: string | undefined) => string;
--- dist/utils.js
+++
@@ -1,4 +0,0 @@
-export var makeType = function (prefix, suffix) {
-    return "" + prefix + (suffix ? "/" + suffix : "");
-};
-//# sourceMappingURL=utils.js.map
