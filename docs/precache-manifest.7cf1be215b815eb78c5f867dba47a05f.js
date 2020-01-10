e0de5a610d8c415311b51ce5e60c3eaa83bcb703
me@jamesmfriedman.com <me@jamesmfriedman.com>
2020-01-10 10:09:50 -0500

    Building docs

---
+++ docs/precache-manifest.7cf1be215b815eb78c5f867dba47a05f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "964aa1c755a172efd9efd8cb859c260a",
+    "url": "/redux-state-branch/index.html"
+  },
+  {
+    "revision": "24c09f79307f4be966c8",
+    "url": "/redux-state-branch/static/css/2.d4d9e415.chunk.css"
+  },
+  {
+    "revision": "cc31d6960b594709d26d",
+    "url": "/redux-state-branch/static/css/main.f755f463.chunk.css"
+  },
+  {
+    "revision": "24c09f79307f4be966c8",
+    "url": "/redux-state-branch/static/js/2.d5d609b2.chunk.js"
+  },
+  {
+    "revision": "b7f5922d7d4b961d3e80e282ba36ae02",
+    "url": "/redux-state-branch/static/js/2.d5d609b2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc31d6960b594709d26d",
+    "url": "/redux-state-branch/static/js/main.6b5fdc63.chunk.js"
+  },
+  {
+    "revision": "aa9c1bff90698568a06b",
+    "url": "/redux-state-branch/static/js/runtime-main.3a73443a.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/redux-state-branch/precache-manifest.0c2f181e2ff9ce186d1ca88cbf175473.js"
+  "/redux-state-branch/precache-manifest.7cf1be215b815eb78c5f867dba47a05f.js"
 );
 
 self.addEventListener('message', (event) => {
